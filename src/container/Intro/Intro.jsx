49007c95df26ffdad47e63b57b6066994903aaeb
phminh3033 <phminh3033@gmail.com>
2022-03-27 08:56:18 +0700

    chef

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import {BsFillPlayFill, BsPauseFill} from 'react-icons/bs';
 
+import { meal } from '../../constants';
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const vidRef = React.useRef();
+  
+  return(
+    <div className="app__video">
+      <video
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        
+      </div>
+    </div>
+  )
+}
 
 export default Intro;
