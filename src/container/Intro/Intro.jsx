348c9047c3d74177351ffe8574f7f344bfca5ca8
phminh3033 <phminh3033@gmail.com>
2022-03-27 14:58:36 +0700

    laurels

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -7,6 +7,16 @@ import './Intro.css';
 const Intro = () => {
   const [playVideo, setPlayVideo] = React.useState(false);
   const vidRef = React.useRef();
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+    if(playVideo){
+      vidRef.current.pause();
+    }else{
+      vidRef.current.play();
+    }
+  }
   
   return(
     <div className="app__video">
@@ -19,7 +29,15 @@ const Intro = () => {
         muted
       />
       <div className="app__video-overlay flex__center">
-        
+        <div 
+          className="app__video-overlay_circle flex__center"
+          onClick={handleVideo}
+        >
+          {playVideo 
+            ? <BsPauseFill color="#FFF" fontSize={30} />
+            : <BsFillPlayFill color="#FFF" fontSize={30} />
+          }
+        </div>
       </div>
     </div>
   )
